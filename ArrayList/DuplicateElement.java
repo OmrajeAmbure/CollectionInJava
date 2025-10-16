421ef441fa40c176aaea7ebff1e1dc07615c5f11
OmrajeAmbure <omrajeambure27@gmail.com>
2025-10-16 10:47:59 +0530

    Today work

---
+++ ArrayList/DuplicateElement.java
@@ -0,0 +1,46 @@
+package ArrayList;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class DuplicateElement {
+        public static ArrayList<Integer> findDuplicates(int[] arr) {
+            HashSet<Integer> set = new HashSet<>();
+            ArrayList<Integer> ans = new ArrayList<>();
+            /**
+             *  Adds the specified element to this set if it is not already present
+             **/
+            for (int i:arr){
+                if (set.add(i) != true){
+                    ans.add(i);
+                }
+            }
+            return ans ;
+        }
+        /**
+         *    1. init empty ans list
+         *    2. Start With outer Loop 0 to n iterate
+         *    2.  Start inner loop  outerLoop +1  to n iterate
+         *    3.  if  arr[outerLoop] == arr[innerLoop ] ans.add(arr[outerLoop])
+         *    4. continue until all duplicate are stored in ans
+         *    5. return the ans
+        **/
+        public static ArrayList<Integer> findDuplicatesNative(int[] arr) {
+        // code here
+            ArrayList<Integer> ans = new ArrayList<>();
+            for(int i=0;i<arr.length;i++){
+                for (int j=i+1;j<arr.length;j++){
+                    if (arr[i] == arr[j]){
+                        ans.add(arr[i]);
+                    }
+                }
+            }
+            return ans;
+        }
+    public static void main(String args[]) {
+            int arr[] = {1,2,3,4,5,6,7,1,2};
+            System.out.println(findDuplicatesNative(arr));
+            System.out.println(findDuplicates(arr));
+    }
+}
+
--- Pattern/SlidingWindow.java
+++ Pattern/SlidingWindow.java
@@ -2,7 +2,7 @@ package Pattern;
 
 public class SlidingWindow {
    public static int maxOfSubArray(int arr[],int k){
-       int current = 0;
+        int current = 0;
         for(int i=0;i<k;i++){
             current += arr[i];
         }
@@ -15,9 +15,25 @@ public class SlidingWindow {
         }
         return maxx;
     }
+//    public static int minOfSubArray(int arr[],int k){
+//
+//       int current  = 0;
+//       for (int i=0;i<k;i++){
+//           current += arr[i];
+//       }
+//        int min = current;
+//       for(int i=0;i<arr.length - k;i++){
+//           current = current - arr[i-1] + arr[i+k-1];
+//           if (current>min){
+//               min = current;
+//           }
+//       }
+//       return min;
+//    }
     public static void main(String[] args) {
         int arr[] = {1,2,3,4,5,6,7,8};
         int k = 3;
         System.out.println(maxOfSubArray(arr,k));
+//        System.out.println(minOfSubArray(arr,k));
     }
 }
